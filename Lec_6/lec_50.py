2eb8fa799b6e8cc5a28daab281dc27c9a6159f3e
do7urden <onur.dvm@gmail.com>
2021-11-13 20:39:51 +0300

    Latest

--- Lec_6/lec_50.py
+++ Lec_6/lec_50.py
@@ -1,4 +1,11 @@
-Bir key birden fazla value yu işaret edemez. Key ler biricik olmalı.
+# Bir key birden fazla value yu işaret edemez. Key ler biricik olmalı.
 
-Master mind nedir araştır. Dictonary de bunu kullancağız.
+# Master mind nedir araştır. Dictonary de bunu kullancağız.
 
+sample_dict_list = [{'Hits': 1, 'Name': 'A'}, {'Hits': 2, 'Name': 'B'}, {'Hits': 3, 'Name': 'C'}, {'Hits': 4, 'Name': 'D'}, {'Hits': 5, 'Name': 'E'}, {'Hits': 6, 'Name': 'NONE'}]
+
+
+result = {x['Name'] : x['Hits'] for x in sample_dict_list if x['Name'] != 'NONE'}
+print(result)
+
+print('###break##')
