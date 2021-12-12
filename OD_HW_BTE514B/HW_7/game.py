9f87aae9b6e0b2d655b6c89c33611be1791f1044
do7urden <onur.dvm@gmail.com>
2021-12-12 19:58:20 +0300

    Random Comp Board Finis!

--- OD_HW_BTE514B/HW_7/game.py
+++ OD_HW_BTE514B/HW_7/game.py
@@ -4,7 +4,7 @@ blankBoard_columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',]
 blankBoard_rows = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-class CompBoard():
+class CompBoard:
 
     name = 'AI'
 
